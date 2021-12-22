1397146769b6eeb8aa30b98dde198103276ac22b
Vladimir Puzakov <puzakov@tochka.com>
2021-12-22 19:11:02 +0300

    chore: use only db url from env

--- weedly_app/config.py
+++ weedly_app/config.py
@@ -1,9 +1,3 @@
 import os
 
-basedir = os.path.dirname(__file__)
-basedir = os.path.join(basedir, '', 'db')
-
-# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '', 'weedly.db') # локальная sqlite
-
-SQLALCHEMY_DATABASE_URI = os.getenv('DB_URL', 'sqlite:///' + os.path.join(basedir, '', 'weedly.db')) # DB_URL прописан в docker-compose
-#'postgresql://example:example@127.0.0.1:5432/mydatabase'
+SQLALCHEMY_DATABASE_URI = os.environ['DB_URL']
