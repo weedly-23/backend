7973c8593aad955a8df2a3e214fae64ef08151fc
Vladimir Puzakov <puzakov@tochka.com>
2022-01-06 22:54:08 +0300

    chore: move db funs to research

--- weedly/app.py
+++ weedly/app.py
@@ -1,22 +1,18 @@
-from flask import Flask, request
+from flask import Flask, jsonify
 
 from weedly.db import models
-from weedly.db.db_funs import DataGetter
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
     models.db.init_app(app)
 
-    getter = DataGetter()
-
-    @app.route('/api/v2/feeds/<string:feed_name>/authors/', methods=['GET'])
-    def get_all_authors_of_a_feed(feed_name):
-        '''получить все статьи по названию фида (meduza, wired, vc)'''
-
-        return {'result' : getter.get_articles_of_a_feed(feed_name)}
-
+    @app.route('/api/v1/feeds/<feed_name>/authors/', methods=['GET'])
+    def get_feed_authors(feed_name: str):
+        return jsonify([])
 
     return app
+
 
 app = create_app()
