f956571b0c7f6aa7af412e8d857b7ed58a44fc53
Vladimir Puzakov <puzakov@tochka.com>
2022-01-06 18:17:22 +0300

    chore: remove sqlite news.db and initialization code

--- weedly/config.py
+++ weedly/config.py
@@ -1,9 +1,4 @@
 import os
 
-# SQLALCHEMY_DATABASE_URI = os.environ['DB_URL']
-# SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-basedir = os.path.dirname(__file__)
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '', 'news.db')
-
-
+SQLALCHEMY_DATABASE_URI = os.environ['DB_URL']
+SQLALCHEMY_TRACK_MODIFICATIONS = False
