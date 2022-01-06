62a55ab96f26786003a0742e289218f80e35b523
Vladimir Puzakov <puzakov@tochka.com>
2022-01-06 19:48:33 +0300

    chore: add app host and port to config

--- weedly/__main__.py
+++ weedly/__main__.py
@@ -1,5 +1,4 @@
-from weedly import app
-
+from weedly import app, config
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host=config.APP_HOST, port=config.APP_PORT, debug=config.DEBUG)
--- weedly/config.py
+++ weedly/config.py
@@ -1,4 +1,10 @@
 import os
 
+# db settings
 SQLALCHEMY_DATABASE_URI = os.environ['DB_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# server settings
+DEBUG = bool(os.getenv('DEBUG', 'False'))
+APP_PORT = int(os.getenv('APP_PORT', '5000'))
+APP_HOST = os.getenv('APP_HOST', '0.0.0.0')
