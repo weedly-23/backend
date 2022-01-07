d71ce21c405af946fb73bbf3dbb715127f2f0343
Vladimir Puzakov <puzakov@tochka.com>
2022-01-07 16:56:47 +0300

    add app error handler

--- weedly/app.py
+++ weedly/app.py
@@ -1,7 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 from weedly.db import models
+from weedly.errors import AppError
 from weedly.views import feeds, users
+
+
+def handle_app_error(error: AppError):
+    return {'error': str(error)}, error.status
 
 
 def create_app():
@@ -11,6 +16,7 @@ def create_app():
 
     app.register_blueprint(feeds.routes, url_prefix='/api/v1/feeds/')
     app.register_blueprint(users.routes, url_prefix='/api/v1/users/')
+    app.register_error_handler(AppError, handle_app_error)
 
     return app
 
--- weedly/errors.py
+++ weedly/errors.py
@@ -1,13 +1,21 @@
+from http import HTTPStatus
 from typing import Any
 
 
 class AppError(Exception):
-    """Application errors."""
+
+    def __init__(self, reason: str, status: HTTPStatus) -> None:
+        super().__init__(reason)
+        self.status = status
 
 
 class NotFoundError(AppError):
+    status = HTTPStatus.NOT_FOUND
 
     def __init__(self, entity: str, uid: Any) -> None:
-        super().__init__(f'Not found {entity} [{uid}]')
+        super().__init__(
+            reason=f'Not found {entity} [{uid}]',
+            status=self.status,
+        )
         self.entity = entity
         self.uid = uid
