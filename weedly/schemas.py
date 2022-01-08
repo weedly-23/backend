d3fd2dbc738ee3e49c69c59d6eedd8438436c1df
Vladimir Puzakov <puzakov@tochka.com>
2022-01-09 02:38:18 +0300

    remove main.py from module

--- weedly/schemas.py
+++ weedly/schemas.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from pydantic import BaseModel, AnyUrl, ValidationError
+from pydantic import AnyUrl, BaseModel
 
 
 class Model(BaseModel):
@@ -30,18 +30,3 @@ class Article(Model):
     name: str
     url: str
     published: datetime
-
-
-if __name__ == '__main__':
-
-    urls = ['google.com', 'asd']
-
-    # for e in urls:
-    j = """
-    {
-    "uid":"1",
-    "name":"my_name",
-    "url": "pydantic-docs.helpmanual.io/usage/types/#urls" }
-    """
-    f = Feed.parse_raw(j)
-    print(f.json())
