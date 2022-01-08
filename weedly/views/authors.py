b390a95370afa203d20c4abbff9b06a3870a3229
Vladimir Puzakov <puzakov@tochka.com>
2022-01-09 02:42:18 +0300

    use abort for no payload error

--- weedly/views/authors.py
+++ weedly/views/authors.py
@@ -1,26 +1,30 @@
-from flask import Blueprint, jsonify, request
-from weedly.repos.authors import AuthorRepo
-from weedly.db.session import db_session
-from weedly import schemas
+from http import HTTPStatus
 
+from flask import Blueprint, abort, jsonify, request
+
+from weedly import schemas
+from weedly.db.session import db_session
+from weedly.repos.authors import AuthorRepo
 
 routes = Blueprint('authors', __name__)
 
 repo = AuthorRepo(db_session)
 
+
 @routes.get('/')
 def get_all():
-    pass
+    return jsonify([]), 200
 
-@routes.put('/')
-def add_author():
+
+@routes.post('/')
+def add():
     payload = request.json
     if not payload:
-        return {"error":"payload required"}, 404
-    payload['uid'] = -1
+        abort(HTTPStatus.BAD_REQUEST, 'payload required')
+
+    payload['uid'] = 0
 
     author = schemas.Author(**payload)
     entity = repo.add(name=author.name, feed_id=author.feed_id)
     new_author = schemas.Author.from_orm(entity)
     return new_author.dict(), 200
-
