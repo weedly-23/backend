ca87393955c962a699bf5c08612bfe7a32faf48c
Vladimir Puzakov <puzakov@tochka.com>
2022-01-09 03:38:20 +0300

    remove fake data generator from project

--- weedly/db/__main__.py
+++ weedly/db/__main__.py
@@ -1,15 +1,19 @@
-from typer import Typer
 import logging
+
+from typer import Typer
+
 from weedly.db.session import Base, engine
 
 app = Typer()
 logger = logging.getLogger(__name__)
 logger.setLevel('DEBUG')
 
+
 @app.command()
 def create():
     Base.metadata.create_all(bind=engine)
     logger.warning('Создали базку!')
+
 
 if __name__ == '__main__':
     app()
