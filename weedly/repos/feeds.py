f905b2631848753d3802d74ee9851dab1cba2179
Vladimir Puzakov <puzakov@tochka.com>
2022-01-09 03:29:48 +0300

    fix feeds repo

--- weedly/repos/feeds.py
+++ weedly/repos/feeds.py
@@ -1,9 +1,11 @@
+import logging
+
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
-from weedly.db.models import Feed, Author, Article
-from weedly.errors import NotFoundError, AlreadyExistsError
+from weedly.db.models import Article, Author, Feed
+from weedly.errors import AlreadyExistsError, NotFoundError
 
-import logging
 logger = logging.getLogger(__name__)
 
 
@@ -12,20 +14,17 @@ class FeedRepo:
     def __init__(self, session: Session) -> None:
         self.session = session
 
+    def add(self, name: str, url: str, is_rss: bool, category: str) -> Feed:
+        feed = Feed(name=name, url=url, is_rss=is_rss, category=category)
 
-    def add(self, name: str, url: str, is_rss: bool, is_deleted=False, category="news") -> Feed:
-        query = self.session.query(Feed)
-        feed = query.filter(Feed.url==url)
+        try:
+            self.session.add(feed)
+            self.session.commit()
+        except IntegrityError as err:
+            raise AlreadyExistsError(entity='feeds', constraint=str(err))
 
-        if feed.count():
-            logger.error('Feed already exists')
-            raise AlreadyExistsError(feed.first().url, feed.first().uid)
-
-        new_feed = Feed(name=name, url=url, is_rss=is_rss, is_deleted=is_deleted, category=category)
-        self.session.add(new_feed)
-        self.session.commit()
         logger.debug('Feed %s добавлен в БД', url)
-        return new_feed
+        return feed
 
     def get_by_id(self, uid: int) -> Feed:
         query = self.session.query(Feed)
@@ -43,15 +42,16 @@ class FeedRepo:
         query = query.limit(limit).offset(offset)
         return query.all()
 
-    def update(self, uid: int, name='', category='',
-               url='', is_rss='',is_deleted='') -> Feed:
+    def update(self, uid: int, name: str, category: str, url: str, is_rss: bool) -> Feed:
         feed = self.get_by_id(uid)
-        args = locals()
-        args.pop('uid')
-        for k, v in args.items():
-            if v:
-                setattr(feed, k, v)
-                self.session.commit()
+
+        feed.name = name
+        feed.category = category
+        feed.url = url
+        feed.is_rss = is_rss
+
+        self.session.commit()
+
         logger.debug('обновили данные для %S', feed)
         return feed
 
@@ -66,26 +66,10 @@ class FeedRepo:
         self.session.commit()
         logger.debug('удалили Feed %S', feed)
 
-    def get_feed_authors(self, uid) -> list[Author]:
+    def get_authors(self, uid) -> list[Author]:
         feed = self.get_by_id(uid)
-        authors = feed.feed_authors
-        if authors:
-            return authors
+        return feed.feed_authors
 
-        logger.warning('Feed %s не имеет авторов', feed)
-        raise NotFoundError('feed.feed_authors', feed.uid)
-
-    def get_feed_articles(self, uid) -> list[Article]:
+    def get_articles(self, uid) -> list[Article]:
         feed = self.get_by_id(uid)
-        articles = feed.feed_articles
-        if articles:
-            return articles
-
-        logger.warning('Feed %s не имеет материалов')
-        raise NotFoundError('feed.feed_materials', feed.uid)
-
-
-if __name__ == '__main__':
-    from weedly.db.session import db_session
-    f = FeedRepo(db_session)
-    f.get_feed_articles(1)
+        return feed.feed_articles
