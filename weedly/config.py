50bd8d81ee0eac28e23dda2885a42d34ad99ba6d
kbondar17 <ironbondar@gmail.com>
2022-01-20 18:51:50 +0300

    новые методы для добавления юзера и rss подписок

--- weedly/config.py
+++ weedly/config.py
@@ -2,11 +2,10 @@ import logging
 import os
 
 # db settings
-# SQLALCHEMY_DATABASE_URI = os.environ['DB_URL']
-# SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = os.environ['DB_URL']
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-basedir = os.path.dirname(__file__)
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '', 'news.db')
+
 # server settings
 DEBUG = bool(os.getenv('DEBUG', 'False'))
 APP_PORT = int(os.getenv('APP_PORT', '5000'))
