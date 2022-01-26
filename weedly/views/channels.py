9c785c3ba225ded28fca284f112b0d9ea23feadd
Vladimir Puzakov <puzakov@tochka.com>
2022-01-26 19:31:08 +0300

    chore: channel views - use http status

--- weedly/views/channels.py
+++ weedly/views/channels.py
@@ -1,9 +1,10 @@
 from http import HTTPStatus
 
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, request
 
 from weedly import schemas
 from weedly.db.session import db_session
+from weedly.jsonify import jsonify
 from weedly.repos.channels import ChannelRepo
 
 routes = Blueprint('channels', __name__)
@@ -16,7 +17,7 @@ def get_all():
     entities = repo.get_all()
     channels = [schemas.Channel.from_orm(entity).dict() for entity in entities]
 
-    return jsonify(channels), 200
+    return jsonify(channels), HTTPStatus.OK
 
 
 @routes.get('/<int:uid>')
@@ -24,7 +25,7 @@ def get_by_uid(uid):
     channel = repo.get_by_uid(uid)
     data = schemas.Channel.from_orm(channel).json()
 
-    return data, 200
+    return data, HTTPStatus.OK
 
 
 @routes.get('/channel-id/<string:channel_id>')
@@ -32,14 +33,14 @@ def get_by_channel_id(channel_id):
     channel = repo.get_by_channel_id(channel_id)
     data = schemas.Channel.from_orm(channel).json()
 
-    return data, 200
+    return data, HTTPStatus.OK
 
 
 @routes.post('/')
 def add():
     payload = request.json
     if not payload:
-        return {'error': 'payload required'}, 400
+        return {'error': 'payload required'}, HTTPStatus.BAD_REQUEST
     payload['uid'] = 0
     channel = schemas.Channel(**payload)
     entity = repo.add(
@@ -53,10 +54,10 @@ def add():
 @routes.delete('/<int:uid>')
 def delete(uid: int):
     repo.delete(uid)
-    return '', 204
+    return '', HTTPStatus.NO_CONTENT
 
 
 @routes.delete('/channel-id/<string:channel_id>')
 def delete_by_channel_id(channel_id: str):
     repo.delete_by_channel_id(channel_id)
-    return '', 204
+    return '', HTTPStatus.NO_CONTENT
